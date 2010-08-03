e9e51c21986f3aa99c62f89ff77ec59b1ef438c9
Jeff Schenck <jmschenck@gmail.com>
2010-08-03 13:37:27 -0700

    Fixes issue with sessions on redirects

--- flatcontent/middleware.py
+++ flatcontent/middleware.py
@@ -4,9 +4,10 @@ class NewUserMiddleware(object):
     session middleware in the stack to work properly. 
     """
     def process_request(self, request):
-        if 'new_user' not in request.session:
+        if hasattr(request, 'session') and 'new_user' not in request.session:
             request.session['new_user'] = True
 
     def process_response(self, request, response):
-        request.session['new_user'] = False
+        if hasattr(request, 'session'):
+            request.session['new_user'] = False
         return response

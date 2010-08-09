f67382c4b74f96af7dd0f375f3fea8386ea81a87
Jeff Schenck <jmschenck@gmail.com>
2010-08-09 00:24:26 -0700

    Hacks the artists index view to redirect to the first artist page

--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
@@ -6,10 +8,14 @@ from artists.models import Artist
 
 def index(request):
     """List of all artists."""
-    artists = Artist.objects.filter(is_active=True).all()
-    return render_to_response('artists/index.html', {
-        'artists': artists,
-    }, context_instance=RequestContext(request))
+    # Temporary hack -- artists index page should redirect to the "first" artist page
+    artist = Artist.objects.filter(is_active=True)[0]
+    return HttpResponseRedirect(reverse('artists.views.artist', kwargs={'artist_slug': artist.slug}))
+    ### The following is the real index view if you ever need it
+    # artists = Artist.objects.filter(is_active=True).all()
+    # return render_to_response('artists/index.html', {
+    #     'artists': artists,
+    # }, context_instance=RequestContext(request))
 
 def artist(request, artist_slug=None):
     """Detail page for an artist."""

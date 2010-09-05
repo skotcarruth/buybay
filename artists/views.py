7981953e2ab7948ed4a7461255c3bec139bc2684
Jeff Schenck <jmschenck@gmail.com>
2010-09-05 16:54:55 -0700

    Revives artist index page

--- artists/views.py
+++ artists/views.py
@@ -8,14 +8,10 @@ from artists.models import Artist
 
 def index(request):
     """List of all artists."""
-    # Temporary hack -- artists index page should redirect to the "first" artist page
-    artist = Artist.objects.filter(is_active=True)[0]
-    return HttpResponseRedirect(reverse('artists.views.artist', kwargs={'artist_slug': artist.slug}))
-    ### The following is the real index view if you ever need it
-    # artists = Artist.objects.filter(is_active=True).all()
-    # return render_to_response('artists/index.html', {
-    #     'artists': artists,
-    # }, context_instance=RequestContext(request))
+    artists = Artist.objects.filter(is_active=True).all()
+    return render_to_response('artists/index.html', {
+        'artists': artists,
+    }, context_instance=RequestContext(request))
 
 def artist(request, artist_slug=None):
     """Detail page for an artist."""

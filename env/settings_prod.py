a67f0b1634d3c3fcefcb75c4f3a2eacfdfec9b33
Jeff Schenck <jmschenck@gmail.com>
2010-09-27 01:47:56 -0700

    Adds prod database settings

--- env/settings_prod.py
+++ env/settings_prod.py
@@ -2,5 +2,16 @@
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'buybay',
+        'USER': 'www',
+        'PASSWORD': 'buybay',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 PAYPAL_SERVER = 'https://api-3t.paypal.com/nvp'
 PAYPAL_CHECKOUT_URL = 'https://www.paypal.com/webscr'
